618291387e49f4ed6c5d27a8a13d92122c0b0449
shaxuzb <113658910+shaxuzb@users.noreply.github.com>
2026-09-14 15:18:11 +0300

    refactor: remove GitHub app version config URL

--- src/modules/app-update/constants/appUpdate.constants.ts
+++ src/modules/app-update/constants/appUpdate.constants.ts
@@ -1,6 +1,3 @@
-export const APP_VERSION_CONFIG_URL =
-  "https://raw.githubusercontent.com/shaxuzb/nasiyadaftarapp/main/app-version.json";
-
 export const APP_UPDATE_REQUEST_TIMEOUT_MS = 8_000;
 export const APP_UPDATE_CHECK_INTERVAL_MS = 15 * 60 * 1_000;
 export const DISMISSED_UPDATE_VERSION_KEY = "dismissed_update_version_v1";
